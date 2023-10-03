fd7a661be02336058b0cfb4e8798c0c080a0e2b6
JoseMPT <black5hole55@gmail.com>
2023-10-02 20:25:52 -0600

    Passed first test for firebase_auth_service.dart.

--- lib/configuration/router/router_configuration.dart
+++ lib/configuration/router/router_configuration.dart
@@ -1,10 +1,13 @@
 import 'package:academic_advice_app/view/home_view.dart';
 import 'package:academic_advice_app/view/login_view.dart';
 import 'package:academic_advice_app/view/register_view.dart';
+import 'package:academic_advice_app/view/test_view.dart';
 import 'package:go_router/go_router.dart';
 
-final routerConfig = GoRouter(initialLocation: '/', routes: [
+final routerConfig = GoRouter(initialLocation: '/test', routes: [
   GoRoute(path: '/', builder: (context, state) => const HomeView()),
   GoRoute(path: '/login_view', builder: (context, state) => const LoginView()),
   GoRoute(path: '/register_view', builder: (context, state) => const RegisterView()),
+
+  GoRoute(path: '/test', builder: (context, state) => const TestView())
 ]);
---
+++ lib/controller/test_controller.dart
@@ -0,0 +1,18 @@
+import 'package:academic_advice_app/model/services/firebase_auth_service.dart';
+
+Future<String> signIn(String email, String password) async {
+  return await signInUser(email, password) ?? 'Success';
+}
+
+Future<String> signUp(String email, String password) async {
+  return await signUpUser(email, password) ?? 'Success';
+}
+
+Future<void> signOut() async {
+  await signOutUser();
+}
+
+Future<String> getUser() async {
+  String? email = getCurrentUser()?.email;
+  return email ?? 'User no sign-in.';
+}
